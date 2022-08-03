0190ae5909348193de62b9347703e5cb6d62e3d3
Guilherme Castelão <guilherme@castelao.net>
2022-08-03 18:10:49 -0300

    feat: MT-Message

--- src/mt/mod.rs
+++ src/mt/mod.rs
@@ -68,3 +68,28 @@ impl InformationElement {
         }
     }
 }
+
+#[derive(Debug)]
+struct MTMessage {
+    elements: Vec<InformationElement>,
+}
+
+// Let's allow dead while still WIP
+#[allow(dead_code)]
+impl MTMessage {
+    fn write<W: std::io::Write>(&self, wtr: &mut W) -> Result<usize, Error> {
+        // Protocol version
+        wtr.write_u8(1)?;
+        let n: usize = self.elements.iter().map(|e| e.len()).sum();
+        wtr.write_u16::<BigEndian>(
+            n.try_into()
+                .expect("Sum of MT information elements lengths is longer than u16"),
+        )?;
+
+        let mut n = 3;
+        for e in &self.elements {
+            n += e.write(wtr)?;
+        }
+        Ok(n)
+    }
+}
