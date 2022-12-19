9b4dcc12c51ef80b506f7acd0392ba3287f3c8be
Guilherme Castelao <guilherme@castelao.net>
2022-12-18 20:19:56 -0700

    Redundant total_size

--- src/mo/mod.rs
+++ src/mo/mod.rs
@@ -57,13 +57,7 @@ impl InformationElement for InformationElementType {
         }
     }
 
-    /// Total size of Information Element in bytes
-    /// This includes the identifier and the field len.
-    fn total_size(&self) -> usize {
-        3 + usize::from(self.len())
-    }
-
-    fn write<W: std::io::Write>(&self, wtr: &mut W) -> Result<usize, Error> {
+    fn write<W: std::io::Write>(&self, wtr: &mut W) -> Result<usize> {
         match self {
             InformationElementType::H(element) => element.write(wtr),
             InformationElementType::P(element) => element.write(wtr),
