34417c25be4bd8631585bd307c8a22d190ab602d
Guilherme Castelao <guilherme@castelao.net>
2023-01-07 19:05:57 -0700

    Some notes to think about

--- directip-dump/src/main.rs
+++ directip-dump/src/main.rs
@@ -3,6 +3,20 @@ use clap::Parser;
 
 use directip::Message;
 
+/* Possible uses/combinations. Think about it.
+ *
+ * Use ncdump as a reference for API behavior
+ *
+ * - default: show everything
+ * --direction: MT or MO
+ * --imei: 012345678901234
+ * --imei-hex: 00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e
+ * --payload=hex
+ * --payload=bin
+ * --payload=int
+ *
+ */
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
