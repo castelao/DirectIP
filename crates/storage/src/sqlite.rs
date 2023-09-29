eec134512480ef72f646594e428925a99d068559
Guilherme Castelão <guilherme@castelao.net>
2023-09-29 11:21:52 -0700

    fix: Syntax mistakes in sqlite module

--- crates/storage/src/sqlite.rs
+++ crates/storage/src/sqlite.rs
@@ -16,7 +16,14 @@ impl super::Storage for SQLiteStorage {
 impl SQLiteStorage {
     pub async fn connect() -> Result<SQLiteStorage, Box<dyn std::error::Error>> {
         let pool = sqlx::SqlitePool::connect("sqlite://:memory:").await?;
-        SQLiteStorage { pool }
+        Ok(SQLiteStorage { pool })
+    }
+
+    pub(super) async fn save(&self, msg: Message) {
+        let _ = sqlx::query("INSERT INTO inbox (payload) VALUES ($1)")
+            .bind(msg.to_vec())
+            .execute(&self.pool)
+            .await;
     }
 
     async fn save(&self, msg: Message) {
