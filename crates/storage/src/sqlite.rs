0e594156c089753e7714adc8696f1422e1f01465
Guilherme Castelão <guilherme@castelao.net>
2023-09-28 19:51:45 -0700

    sqlite with async

--- crates/storage/src/sqlite.rs
+++ crates/storage/src/sqlite.rs
@@ -5,13 +5,26 @@ use tokio;
 
 use directip::Message;
 
+#[derive(Debug)]
 pub struct SQLiteStorage {
     pool: sqlx::SqlitePool,
 }
 
 impl super::Storage for SQLiteStorage {
-    fn save(&mut self, msg: Message) {
-        unimplemented!()
+}
+
+impl SQLiteStorage {
+    pub async fn connect() -> Result<SQLiteStorage, Box<dyn std::error::Error> {
+        let pool = sqlx::SqlitePool::connect("sqlite://:memory:")
+            .await?;
+        SQLiteStorage { pool }
+    }
+
+    async fn save(&self, msg: Message) {
+            sqlx::query("INSERT INTO inbox (payload) VALUES ($1)")
+                .bind(msg.to_vec())
+                .execute(&self.pool)
+                .await;
     }
 }
 
