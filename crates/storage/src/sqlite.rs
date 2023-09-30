e090ab6cd87dc4e83f54dcd6551d6bd84b9a518b
Guilherme Castelão <guilherme@castelao.net>
2023-09-29 22:44:06 -0700

    Testing sqlite

--- crates/storage/src/sqlite.rs
+++ crates/storage/src/sqlite.rs
@@ -14,7 +14,17 @@ impl super::Storage for SQLiteStorage {}
 
 impl SQLiteStorage {
     pub async fn connect() -> Result<SQLiteStorage, Box<dyn std::error::Error>> {
+        // let pool = SqlitePool::connect("mssql://").await?;
         let pool = sqlx::SqlitePool::connect("sqlite://:memory:").await?;
+
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS inbox (
+                payload BLOB);",
+        )
+        .execute(&pool)
+        .await
+        .unwrap();
+
         Ok(SQLiteStorage { pool })
     }
 
@@ -23,5 +33,28 @@ impl SQLiteStorage {
             .bind(msg.to_vec())
             .execute(&self.pool)
             .await;
+    }
+}
+
+#[cfg(test)]
+mod test_sqlite {
+    use super::*;
+
+    fn sample() -> directip::Message {
+        let msg = directip::mt::MTMessage::from_reader(
+            [
+                0x01, 0x00, 0x1c, 0x44, 0x00, 0x19, 0x00, 0x00, 0x27, 0x0f, 0x00, 0x01, 0x02, 0x03,
+                0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0xff, 0xff, 0xff,
+                0xff, 0xff, 0xf5,
+            ]
+            .as_slice(),
+        );
+        directip::Message::MT(msg.unwrap())
+    }
+
+    #[tokio::test]
+    async fn sqlite_inmemory() {
+        let db = SQLiteStorage::connect().await.unwrap();
+        db.save(sample());
     }
 }
