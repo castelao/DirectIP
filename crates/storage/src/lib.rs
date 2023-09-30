e62cd434784c5812d5f39605296a76f02754067c
Guilherme Castelão <guilherme@castelao.net>
2023-09-29 23:30:34 -0700

    testing inmemory

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -61,11 +61,18 @@ mod tests {
     use super::Database;
 
     fn sample() -> directip::Message {
-        let msg = directip::mt::MTMessage::from_reader([].as_slice());
+        let msg = directip::mt::MTMessage::from_reader(
+            [
+                0x01, 0x00, 0x1c, 0x44, 0x00, 0x19, 0x00, 0x00, 0x27, 0x0f, 0x00, 0x01, 0x02, 0x03,
+                0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0xff, 0xff, 0xff,
+                0xff, 0xff, 0xf5,
+            ]
+            .as_slice(),
+        );
         directip::Message::MT(msg.unwrap())
     }
 
-    //#[tokio::test]
+    #[tokio::test]
     async fn volatile() {
         let db = Database::open("inmemory://").await.unwrap();
         db.save(sample());
