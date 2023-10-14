fad856dd43b9f5967158c7a873ba137d3526b5d8
Gui Castelao <guilherme@castelao.net>
2023-10-14 17:47:09 -0600

    Using common sample for testing
    Avoid repetition.

--- crates/storage/src/inmemory.rs
+++ crates/storage/src/inmemory.rs
@@ -28,19 +28,7 @@ impl VolatileStorage {
 #[cfg(test)]
 mod test_volatile {
     use super::VolatileStorage;
-    use directip;
-
-    fn sample() -> directip::Message {
-        let msg = directip::mt::MTMessage::from_reader(
-            [
-                0x01, 0x00, 0x1c, 0x44, 0x00, 0x19, 0x00, 0x00, 0x27, 0x0f, 0x00, 0x01, 0x02, 0x03,
-                0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0xff, 0xff, 0xff,
-                0xff, 0xff, 0xf5,
-            ]
-            .as_slice(),
-        );
-        directip::Message::MT(msg.unwrap())
-    }
+    use directip::sample;
 
     #[tokio::test]
     async fn volatile() {
