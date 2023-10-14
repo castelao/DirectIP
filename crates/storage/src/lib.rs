77954211c2ea0489fd3e5458ac6bab0299b899bf
Gui Castelao <guilherme@castelao.net>
2023-10-14 08:37:02 -0600

    Creating a common sample at directip
    This will be used for testing derived crates.

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -59,18 +59,7 @@ impl Database {
 #[cfg(test)]
 mod tests {
     use super::Database;
-
-    fn sample() -> directip::Message {
-        let msg = directip::mt::MTMessage::from_reader(
-            [
-                0x01, 0x00, 0x1c, 0x44, 0x00, 0x19, 0x00, 0x00, 0x27, 0x0f, 0x00, 0x01, 0x02, 0x03,
-                0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0xff, 0xff, 0xff,
-                0xff, 0xff, 0xf5,
-            ]
-            .as_slice(),
-        );
-        directip::Message::MT(msg.unwrap())
-    }
+    use directip::sample;
 
     #[tokio::test]
     async fn volatile() {
